be4c02254ffa7de9773ff729de0e4925b1ce7db6
Fearghal O Maolcatha <fearghal.omaolcatha@dell.com>
2023-02-21 10:38:19 +0000

    updates to recreate spinner issue with @angular/material@15

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular15_latest';
+  enabled = false;
+
+  onClick(): void {
+    this.enabled = !this.enabled;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {MatLegacyButtonModule as MatButtonModule} from "@angular/material/legacy-button";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
